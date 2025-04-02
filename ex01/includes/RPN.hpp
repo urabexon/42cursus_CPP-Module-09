f7a6b8f8d38fdfb6e40dde4a90007311ac6c5d9e
urabexon <hiroki708.a48uk@gmail.com>
2025-04-02 14:50:10 +0900

    review fix ex00 add doc

--- ex01/includes/RPN.hpp
+++ ex01/includes/RPN.hpp
@@ -3,14 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   RPN.hpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hurabe <hurabe@student.42.fr>              +#+  +:+       +#+        */
+/*   By: urabex <urabex@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 19:32:58 by hurabe            #+#    #+#             */
-/*   Updated: 2025/03/30 22:37:12 by hurabe           ###   ########.fr       */
+/*   Updated: 2025/04/02 14:49:10 by urabex           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// stack
+/*
+  逆ポーランド記法は後置記法とも呼ばれており、数値と演算子を空白区切りで並べる形式のこと。  
+  この形式では、演算子が出てきたときにそれまでの数値を順に処理していくため、LIFO構造が最適となる。
+  このLIFO構造に自然に対応しているコンテナがstackであり、pushで数値を積み上げてpopで直前の2つの値を取り出して演算を行う
+  RPNに最も適しているため。
+*/
 
 #ifndef RPN_HPP
 #define RPN_HPP
@@ -50,7 +55,7 @@ class RPN {
 		class	SyntaxErrorException : public std::exception {
 			public:
 				virtual const char* what() const throw (){
-					return "syntax error";
+					return "Error";
 				}
 		};
 };
