2c9df51dc1e8a77e5f8042566496c3114eb685c8
urabexon <hiroki708.a48uk@gmail.com>
2025-04-11 17:05:18 +0900

    main setup

--- ex02_list/srcs/main.cpp
+++ ex02_list/srcs/main.cpp
@@ -6,12 +6,21 @@
 /*   By: hurabe <hurabe@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 22:15:55 by hurabe            #+#    #+#             */
-/*   Updated: 2025/04/10 22:15:58 by hurabe           ###   ########.fr       */
+/*   Updated: 2025/04/11 17:04:58 by hurabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 
 int main(int argc, char **argv) {
-    return 0;
+    if (argc < 3) {
+        std::cout << "Error: Not enough argument" << std::endl;
+        return (EXIT_FAILURE);
+    }
+    try {
+        
+    } catch (const char *msg) {
+        
+    }
+    return (EXIT_SUCCESS);
 }
