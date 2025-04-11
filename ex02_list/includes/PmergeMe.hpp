2569079eb80b304978277b432379fbb3b297a78c
urabexon <hiroki708.a48uk@gmail.com>
2025-04-11 16:52:06 +0900

    add canonical form

--- ex02_list/includes/PmergeMe.hpp
+++ ex02_list/includes/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: hurabe <hurabe@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 22:15:40 by hurabe            #+#    #+#             */
-/*   Updated: 2025/04/10 22:17:48 by hurabe           ###   ########.fr       */
+/*   Updated: 2025/04/11 16:48:13 by hurabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,9 +33,18 @@ typedef struct s_pairDeq {
 }	t_pairDeq;
 
 class PmergeMe {
-  private:
+	private:
+		PmergeMe();
+		
 
-  public:
+	public:
+		PmergeMe(int argc, char **argv);
+		PmergeMe(const PmergeMe &src);
+		PmergeMe &operator=(const PmergeMe &src);
+		~PmergeMe();
+		void		excutePmergeMe(int argc, char **argv);
+		static bool compVctElements(const t_pairVct &first, const t_pairVct &second);
+		static bool compDeqElements(const t_pairDeq &first, const t_pairDeq &second);
 };
 
 #endif
