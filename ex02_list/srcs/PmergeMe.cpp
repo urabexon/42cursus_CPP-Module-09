0f76d1897494653a9d63854f81025bedde96f1c5
urabexon <hiroki708.a48uk@gmail.com>
2025-04-11 17:01:47 +0900

    add cpp file canonical form

--- ex02_list/srcs/PmergeMe.cpp
+++ ex02_list/srcs/PmergeMe.cpp
@@ -6,7 +6,31 @@
 /*   By: hurabe <hurabe@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 22:16:07 by hurabe            #+#    #+#             */
-/*   Updated: 2025/04/10 22:16:08 by hurabe           ###   ########.fr       */
+/*   Updated: 2025/04/11 17:01:18 by hurabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "PmergeMe.hpp"
+
+// counter
+//int g_counterVct = 0;
+//int g_counterDeq = 0;
+
+PmergeMe::PmergeMe() : _vct(), _deq() {}
+
+PmergeMe::PmergeMe(int argc, char **argv) : _vct(), _deq() {
+	validate(argc, argv);
+}
+
+PmergeMe::PmergeMe(const PmergeMe &src) : _vct(src._vct), _deq(src._deq) {}
+
+PmergeMe &PmergeMe::operator=(const PmergeMe &src) {
+	if (&src != this) {
+		_vct = src._vct;
+		_deq = src._deq;
+	}
+	return (*this);
+}
+
+PmergeMe::~PmergeMe() {}
 
